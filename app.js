ff777782dfb2d19d4482c69bed3d8189b9592588
surya Vivek Janjam <suryavivek33377@gmail.com>
2021-06-28 19:16:39 +0530

    checks : the operator should be add or operate.

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
 class Calculator {
-  constructor() {}
+  constructor(input) {
+    this.input = input;
+  }
+
+  appendNumber(input, number) {
+    if (number === "." && input.innerText.includes(".")) {
+      return;
+    } else {
+      input.innerText += number;
+    }
+  }
+
+  appendOperation(operator, operations) {
+    this.input.innerText += operator;
+    console.log(input.innerText);
+    this.checkOperation(operator, operations);
+  }
+
+  checkOperation(operator, operations) {
+    let flag = 0;
+    for (let i = 0; i < input.innerText.length; i++) {
+      for (let j = 0; j < operations.length; j++) {
+        if (input.innerText[i] === operations[j]) {
+          flag += 1;
+
+          break;
+        }
+      }
+    }
+    if (flag === 2) {
+      // operate();
+      console.log(`operating the numbers ${operator} and ${flag}`);
+    } else {
+      console.log(`Appending operator ${operator} and ${flag}`);
+    }
+  }
+
+  operate() {
+    // Operations of the numbers goes here...
+  }
   reset(input) {
     input.innerText = ``;
   }
@@ -8,22 +47,39 @@ class Calculator {
     // input.innerText -=  input.innerText.length
   }
 }
+
+//Selections
 const numbers = document.querySelectorAll("[data-number]");
 const input = document.querySelector("[data-input]");
-const operations = document.querySelector("[data-operation]");
+const operations = document.querySelectorAll("[data-operation]");
 const resetBtn = document.querySelector("[data-reset]");
 const deleteBtn = document.querySelector("[data-delete]");
 const equalsBtn = document.querySelector("[data-equals]");
 
+//Converting innerText of operations to an array
+const operatorArray = [];
+Object.values(operations).forEach((ele) => operatorArray.push(ele.innerText));
+
+//Appending numbers
 numbers.forEach((number) => {
-  number.addEventListener("click", (e) => {
-    input.innerText += number.innerText;
+  number.addEventListener("click", () => {
+    calculator.appendNumber(input, number.innerText);
   });
 });
 
-const calculator = new Calculator(input);
+//Appending operators
+operations.forEach((operation) => {
+  operation.addEventListener("click", () => {
+    calculator.appendOperation(operation.innerText, operatorArray);
+  });
+});
 
+//Reset Button
 resetBtn.addEventListener("click", () => calculator.reset(input));
+
+//Delete Button
 deleteBtn.addEventListener("click", () => {
   console.log(input.innerText.length);
 });
+
+const calculator = new Calculator(input);
