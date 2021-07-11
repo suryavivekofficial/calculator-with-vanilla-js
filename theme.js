cd02b6671cdd171b877e62d1f9b974bc62bc0889
surya Vivek Janjam <suryavivek33377@gmail.com>
2021-07-11 19:48:29 +0530

    changes theme

--- theme.js
+++ theme.js
@@ -3,11 +3,14 @@ const buttons = document.querySelectorAll("[data-theme]");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.id === "theme-1") {
-      console.log("theme 1");
+      document.body.className = ``;
+      document.body.classList.add(`theme-1`);
     } else if (button.id === "theme-2") {
-      console.log("theme 2");
+      document.body.className = ``;
+      document.body.classList.add(`theme-2`);
     } else if (button.id === "theme-3") {
-      console.log(`theme 3`);
+      document.body.className = ``;
+      document.body.classList.add(`theme-3`);
     }
   });
 });
