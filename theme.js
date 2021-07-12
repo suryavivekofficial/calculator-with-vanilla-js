a093b2fa4d07bdda6669677956daf02288250e6e
surya Vivek Janjam <suryavivek33377@gmail.com>
2021-07-12 12:17:45 +0530

    loads the selected theme even refreshed

--- theme.js
+++ theme.js
@@ -1,16 +1,20 @@
 const buttons = document.querySelectorAll("[data-theme]");
+const theme = localStorage.getItem(`theme`);
+
+if (theme) {
+  document.body.className = ``;
+  document.body.classList.add(theme);
+  buttons.forEach((button) => {
+    if (button.id === theme) {
+      button.checked = true;
+    }
+  });
+}
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (button.id === "theme-1") {
-      document.body.className = ``;
-      document.body.classList.add(`theme-1`);
-    } else if (button.id === "theme-2") {
-      document.body.className = ``;
-      document.body.classList.add(`theme-2`);
-    } else if (button.id === "theme-3") {
-      document.body.className = ``;
-      document.body.classList.add(`theme-3`);
-    }
+    document.body.className = ``;
+    document.body.classList.add(button.id);
+    localStorage.setItem(`theme`, `${button.id}`);
   });
 });
